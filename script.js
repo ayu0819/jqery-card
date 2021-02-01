54c192c166fceeebcec8f768a6c45df9d9a713e1
ayu0819 <ayutateishi0819@gmail.com>
2021-02-01 23:37:17 +0900

    first_commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.card').hover(function(){
+  if($(this).hasClass("active")){
+      $('.card .bottom').slideUp(function(){
+          $('.card').removeClass("active");
+      });
+  }else{
+    $('.card').addClass("active");
+    $('.card .bottom').stop().slideDown();
+  }
+});
